cc2c14c3e578d937ab6ce37a1e9779fecbeb9fec
Keanu Hie <hie.keanu@gmail.com>
2025-02-19 00:13:41 +0100

    feat: add inputfield molecule

---
+++ src/components/molecules/InputField.vue
@@ -0,0 +1,26 @@
+<template>
+  <div :class="{ 'w-full': containerFullWidth }" class="flex flex-col">
+    <InputLabel :id="id">
+      <slot name="label" />
+    </InputLabel>
+    <InputField :id="id" v-bind="$attrs" @input="emit('input', $event)" @blur="emit('blur')" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import InputLabel from '@/components/atoms/InputLabel.vue'
+import InputField from '@/components/atoms/InputField.vue'
+
+export interface InputFieldProps {
+  id: string
+  containerFullWidth?: boolean
+}
+
+withDefaults(defineProps<InputFieldProps>(), {
+  containerFullWidth: false,
+})
+
+const emit = defineEmits(['input', 'blur'])
+</script>
+
+<style scoped></style>
