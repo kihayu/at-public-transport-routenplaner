c1428968ddec2fba193c855007c598fbfe35b16a
Keanu Hie <hie.keanu@gmail.com>
2025-02-25 17:12:40 +0100

    feat: add import/export functionality

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,40 @@
   <div class="mx-auto max-w-7xl px-4 py-8 sm:px-6 lg:px-8">
     <h1 class="mb-8 text-center text-3xl font-bold text-gray-900">Öffi-Routenplaner</h1>
     <div class="flex flex-col gap-8">
-      <AddressContainer @calculate="handleCalculate" />
+      <div class="max-h-fit rounded-lg bg-white p-6 shadow-md">
+        <div
+          class="my-4 flex flex-col-reverse gap-x-4 gap-y-4 border-b border-gray-300 pb-4 md:flex-row-reverse"
+        >
+          <div
+            class="cursor-pointer rounded-md bg-gray-100 px-4 py-2 text-gray-700 transition-colors duration-200 hover:bg-gray-200"
+          >
+            <button
+              class="mr-2 cursor-pointer border-r border-gray-400 pr-2 disabled:cursor-not-allowed disabled:opacity-50 md:w-fit"
+              :disabled="!importedFile"
+              @click="fillPlan"
+            >
+              Import
+            </button>
+            <input
+              class="w-48 cursor-pointer md:w-fit [&_input]:!cursor-pointer"
+              type="file"
+              @change="importFile"
+              accept=".json"
+            />
+          </div>
+          <button
+            class="cursor-pointer rounded-md bg-gray-100 px-4 py-2 text-gray-700 transition-colors duration-200 hover:bg-gray-200"
+            @click="exportPlan"
+          >
+            Export
+          </button>
+        </div>
+        <AddressContainer
+          :imported-addresses="addresses"
+          @calculate="handleCalculate"
+          @address-export="setAddresses"
+        />
+      </div>
 
       <div v-if="error" class="rounded-md bg-red-100 p-4 text-red-700">
         {{ error }}
@@ -18,10 +51,51 @@ import { useTransitCalculator } from '@/composables/useTransitCalculator'
 import AddressContainer from '@/components/AddressContainer.vue'
 import TransitResults from '@/components/TransitResults.vue'
 import type { AddressDuration } from '@/types/AddressDuration'
+import { ref, type Ref } from 'vue'
 
 const { calculateTransitTimes, results, error } = useTransitCalculator()
 
 const handleCalculate = async (addresses: Array<AddressDuration>, startTime: number) => {
   await calculateTransitTimes(addresses, startTime)
+}
+
+const addresses: Ref<Array<AddressDuration>> = ref([
+  { address: '', duration: 0 },
+  { address: '', duration: 0 },
+])
+const importedFile = ref()
+
+const setAddresses = (newAddresses: Array<AddressDuration>) => {
+  addresses.value = newAddresses
+}
+
+const download = (content: string, filename: string) => {
+  const element = document.createElement('a')
+  const blob = new Blob([content], { type: 'application/json' })
+  element.href = URL.createObjectURL(blob)
+  element.download = filename
+  element.click()
+}
+
+const importFile = (event: Event) => {
+  const target = event.target as HTMLInputElement
+  if (target.files?.length) {
+    importedFile.value = target.files[0]
+  }
+}
+
+const exportPlan = () => {
+  const exportJson = JSON.stringify(addresses.value)
+  download(exportJson, 'plan.json')
+}
+
+const fillPlan = async () => {
+  if (!importedFile.value) {
+    return
+  }
+  const text = await importedFile.value.text()
+  const jsonObject = JSON.parse(text)
+  console.log(jsonObject)
+  addresses.value = jsonObject as Array<AddressDuration>
 }
 </script>
--- src/components/AddressContainer.vue
+++ src/components/AddressContainer.vue
@@ -1,53 +1,57 @@
 <template>
-  <div class="max-h-fit rounded-lg bg-white p-6 shadow-md">
-    <div class="mb-6 space-y-4">
-      <div v-for="(address, index) in addresses" :key="index" class="relative">
-        <AddressInput
-          :address="address"
-          :address-input="addressInputs[index]"
-          :index="index"
-          :show-extensions="index !== 0 && index !== addresses.length - 1"
-          :start-date="startDate"
-          :start-time="startTime"
-          @update:address="updateAddress(index, $event)"
-          @update:address-input="updateAddressInput(index, $event)"
-          @update:start-time="updateStartTime"
-          @update:start-date="updateStartDate"
-          @remove="removeAddress"
-        />
-      </div>
+  <div class="mb-6 space-y-4">
+    <div v-for="(address, index) in addresses" :key="index" class="relative">
+      <AddressInput
+        :address="address"
+        :address-input="addressInputs[index]"
+        :index="index"
+        :show-extensions="index !== 0 && index !== addresses.length - 1"
+        :start-date="startDate"
+        :start-time="startTime"
+        @update:address="updateAddress(index, $event)"
+        @update:address-input="updateAddressInput(index, $event)"
+        @update:start-time="updateStartTime"
+        @update:start-date="updateStartDate"
+        @remove="removeAddress"
+      />
     </div>
-
-    <div class="mt-6 flex gap-4 border-t border-gray-200 pt-6">
-      <button
-        @click="addAddress"
-        class="cursor-pointer rounded-md bg-gray-100 px-4 py-2 text-gray-700 transition-colors duration-200 hover:bg-gray-200"
-        type="button"
-        :disabled="isLoading"
-        :class="{ 'cursor-not-allowed opacity-50': isLoading }"
-      >
-        Adresse hinzufügen
-      </button>
-      <button
-        @click="calculate"
-        class="rounded-md bg-blue-600 px-4 py-2 text-white transition-colors duration-200 hover:bg-blue-700 disabled:hover:bg-blue-600"
-        :disabled="addresses.length < 2 || isLoading || hasEmptyAddresses"
-        :class="{
-          'cursor-not-allowed opacity-50': addresses.length < 2 || isLoading || hasEmptyAddresses,
-          'cursor-pointer': !isLoading && !hasEmptyAddresses,
-        }"
-      >
-        {{ isLoading ? 'Berechne...' : 'Route berechnen' }}
-      </button>
-    </div>
+  </div>
+  <div class="mt-6 flex gap-4 border-t border-gray-200 pt-6">
+    <button
+      @click="addAddress"
+      class="cursor-pointer rounded-md bg-gray-100 px-4 py-2 text-gray-700 transition-colors duration-200 hover:bg-gray-200"
+      type="button"
+      :disabled="isLoading"
+      :class="{ 'cursor-not-allowed opacity-50': isLoading }"
+    >
+      Adresse hinzufügen
+    </button>
+    <button
+      @click="calculate"
+      class="rounded-md bg-blue-600 px-4 py-2 text-white transition-colors duration-200 hover:bg-blue-700 disabled:hover:bg-blue-600"
+      :disabled="addresses.length < 2 || isLoading || hasEmptyAddresses"
+      :class="{
+        'cursor-not-allowed opacity-50': addresses.length < 2 || isLoading || hasEmptyAddresses,
+        'cursor-pointer': !isLoading && !hasEmptyAddresses,
+      }"
+    >
+      {{ isLoading ? 'Berechne...' : 'Route berechnen' }}
+    </button>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref, computed } from 'vue'
+import { ref, computed, watch, onMounted } from 'vue'
 import type { AddressDuration } from '@/types/AddressDuration'
 import AddressInput from '@/components/AddressInput.vue'
 import moment from 'moment'
+import { formatTime } from '@/utils/formatTime'
+
+export interface AddressContainerProps {
+  importedAddresses: Array<AddressDuration>
+}
+
+const props = defineProps<AddressContainerProps>()
 
 interface AddressInputState extends AddressDuration {
   durationInput: string
@@ -55,21 +59,25 @@ interface AddressInputState extends AddressDuration {
 
 const emit = defineEmits<{
   calculate: [addresses: Array<AddressDuration>, startTime: number]
+  addressExport: [adresses: Array<AddressDuration>]
 }>()
 
 const isLoading = ref(false)
 const startTime = ref(moment().format('HH:mm'))
 const startDate = ref(moment().format('YYYY-MM-DD'))
 
-const addresses = ref<Array<AddressDuration>>([
-  { address: '', duration: 0 },
-  { address: '', duration: 0 },
-])
+const addresses = ref<Array<AddressDuration>>(props.importedAddresses)
 
-const addressInputs = ref<Array<AddressInputState>>([
-  { address: '', duration: 0, durationInput: '' },
-  { address: '', duration: 0, durationInput: '' },
-])
+const addressesToAddressInputs = () => {
+  return addresses.value.map((address) => {
+    return {
+      durationInput: formatTime(address.duration.toString()),
+      ...address,
+    } as AddressInputState
+  })
+}
+
+const addressInputs = ref<Array<AddressInputState>>(addressesToAddressInputs())
 
 const hasEmptyAddresses = computed(() => addresses.value.some((address) => !address.address.trim()))
 
@@ -108,4 +116,18 @@ const calculate = () => {
   const startTimeSeconds = Math.floor(dateTime.valueOf() / 1000)
   emit('calculate', validAddresses, startTimeSeconds)
 }
+
+onMounted(() => {
+  addresses.value = props.importedAddresses
+})
+
+watch(
+  () => props.importedAddresses,
+  () => {
+    console.log('set addresses to props')
+    addresses.value = props.importedAddresses
+    addressInputs.value = addressesToAddressInputs()
+  },
+  { deep: true },
+)
 </script>
